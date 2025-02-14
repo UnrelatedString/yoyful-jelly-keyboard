3917c8e489fbdac1e68e43cb96b81cc596b6b9d0
UnrelatedString <unrelatedstring@gmail.com>
2025-02-14 10:35:53 -0500

    WHAT IS WRONG WITH MY TYPESSSSSSSSS

--- src/Main.purs
+++ src/Main.purs
@@ -13,12 +13,13 @@ import Web.HTML (window)
 import Web.HTML.Window (Window, document)
 import Web.HTML.HTMLDocument (body, toDocument)
 import Web.HTML.HTMLElement as HTMLElement
-import Web.DOM.Document (Document, createElement, createTextNode)
+import Web.DOM.Document (Document)
 import Web.DOM.Element (Element)
 import Web.CSSOM.ElementCSSInlineStyle (style, fromHTMLElement)
-import Web.CSSOM.CSSStyleDeclaration (setProperty)
+import Web.CSSOM.CSSStyleDeclaration (CSSStyleDeclaration, setProperty)
 
-import Web.Nice (appendChild)
+import Web.Nice.Node (appendChild)
+import Web.Nice.Builder (Builder, createElement, createText)
 
 main :: Effect Unit
 main = try window >>= either noWindow windowedMain
@@ -36,7 +37,6 @@ windowedMain win = do
   into <- HTMLElement.toNode <$> expect "There's no body 😭😭😭" maybeBody
   keyboard <- buildKeyboard doc
   appendChild into keyboard
-  styleKeyboard keyboard
   celebrateSuccess
 
 celebrateSuccess :: Effect Unit
@@ -45,21 +45,18 @@ celebrateSuccess = log "🍾"
 expect :: forall a. String -> Maybe a -> Effect a
 expect message = throw message `maybe` pure
 
-buildKeyboard :: Document -> Effect Element
-buildKeyboard doc = do
-  keyboard <- createElement "div" doc
+buildKeyboard :: Builder Element
+buildKeyboard = do
+  keyboard <- createElement "div"
   let append = appendChild keyboard
   text <- createTextNode "keyboard go clicky clacky clicky clacky" doc
   append text
   pure keyboard
 
-styleKeyboard :: Element -> Effect Unit
-styleKeyboard keyboard = do
-  let literallyHowIsThisFallible = HTMLElement.fromElement keyboard
+inlineDecl :: Element -> Effect CSSStyleDeclaration 
+inlineDecl elem = do
+  let literallyHowIsThisFallible = HTMLElement.fromElement elem
   htmlElement <- expect
     "Failed to convert Web.DOM.Element to Web.HTML.HTMLElement. SOMEHOW. !?!?!?"
     literallyHowIsThisFallible
-  css <- style $ fromHTMLElement htmlElement
-  -- wait am I going to have to inline style everything?? what about like. selectors. ???
-  setProperty "color" "white" css
-  pure unit
+  style $ fromHTMLElement htmlElement
