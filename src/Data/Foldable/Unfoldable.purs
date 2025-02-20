74cd79325889958a3de40426174e573bbc925e45
UnrelatedString <unrelatedstring@gmail.com>
2025-02-20 11:15:18 -0500

    I feel like a mad scientist rn. you can just FOLD ENUMS NOW

--- src/Data/Foldable/Unfoldable.purs
+++ src/Data/Foldable/Unfoldable.purs
@@ -5,7 +5,7 @@ module Data.Foldable.Unfoldable
 
 import Prelude
 
-import Data.Foldable (class Foldable, foldMap, foldlDefault, foldMapDefaultR)
+import Data.Foldable (class Foldable, foldMap, foldrDefault, foldMapDefaultL)
 import Data.Unfoldable (class Unfoldable, class Unfoldable1, unfoldr)
 import Data.Tuple.Nested ((/\), type (/\))
 import Data.Maybe (Maybe(..))
@@ -26,17 +26,17 @@ instance Unfoldable1 TrivialUnfold where
           adapter = map f
 
 -- can't actually define a Foldable Unfolder instance because the types are backwards :(
-foldrUnfolder :: forall a b c. (a -> c -> c) -> c -> Unfolder a b -> c
-foldrUnfolder f foldInit (Unfolder g unfoldSeed) = lockstep unfoldSeed foldInit
+foldlUnfolder :: forall a b c. (c -> a -> c) -> c -> Unfolder a b -> c
+foldlUnfolder f foldInit (Unfolder g unfoldSeed) = lockstep unfoldSeed foldInit
   where lockstep :: b -> c -> c
         lockstep seed acc
-          | Just (a /\ seed') <- g seed = lockstep seed' $ f a acc
+          | Just (a /\ seed') <- g seed = lockstep seed' $ f acc a
           | otherwise = acc
 
 instance Foldable TrivialUnfold where
-  foldr f b (TrivialUnfold e) = runExists (foldrUnfolder f b) e
-  foldl f = foldlDefault f
-  foldMap f = foldMapDefaultR f
+  foldl f b (TrivialUnfold e) = runExists (foldlUnfolder f b) e
+  foldr f = foldrDefault f
+  foldMap f = foldMapDefaultL f
 
 foldEnum :: forall a b. BoundedEnum a => Monoid b => (a -> b) -> b
 foldEnum f = foldMap f everything
