fdb5795a052c6cec2af142213e71fe446706c0d5
UnrelatedString <unrelatedstring@gmail.com>
2025-02-19 22:10:03 -0500

    just need to save a snapshot of some of The Code Ever comonads are actually pretty sweet. did not see that coming

--- src/Jelly/Sub.purs
+++ src/Jelly/Sub.purs
@@ -6,11 +6,12 @@ import Prelude
 
 import Data.String (stripSuffix, length)
 import Data.String.Pattern (Pattern(..))
-import Data.Maybe (Maybe(..), maybe')
+import Data.Maybe (Maybe(..), fromMaybe')
 import Data.Functor (class Functor)
 import Control.Alternative (class Alt, class Plus, class Alternative, (<|>), empty)
 import Data.Newtype (class Newtype, wrap, unwrap)
 import Control.Monad.Writer (Writer, execWriter, tell)
+import Control.Comonad ((=>>))
 
 type SingleSub = {prefix :: String, sub :: String, deltaLength :: Int}
 
@@ -20,11 +21,17 @@ derive instance Newtype (Substitution a b) _
 infixr 0 type Substitution as ->?
 
 instance Functor (Substitution a) where
-  map f = wrap <<< map f <<< unwrap -- I have NO idea why modify <<< map doesn't typecheck. ???
+  map f = wrap <<< map (map f) <<< unwrap -- I have NO idea why modify <<< map doesn't typecheck. ???
+
+instance Apply (Substitution a) where
+  apply f a = wrap \x -> unwrap f x <*> unwrap a x 
+
+instance Applicative (Substitution a) where
+  pure = wrap <<< const <<< pure
 
 instance Alt (Substitution a) where
   alt a b = wrap \x -> -- Not using Alt Maybe because no laziness means no short circuting!!
-    maybe' (\_ -> unwrap b x) $ unwrap a x
+    unwrap a x =>> identity >>= fromMaybe' (\_ -> unwrap b x)
 
 instance Plus (Substitution a) where
   empty = wrap $ const Nothing
@@ -39,7 +46,7 @@ instance Monoid (Substitution a b) where
 
 makeSubstitution :: String -> String -> String ->? SingleSub
 makeSubstitution pat to = wrap \text -> do
-  prefix <- stripSuffix $ Pattern pat
+  prefix <- stripSuffix text $ Pattern pat
   pure {
     prefix,
     sub: to,
@@ -57,7 +64,7 @@ trySubstitute :: String -> Maybe String
 trySubstitute = unwrap $ tryBatchSubstitute <|> trySingleSubstitutions
 
 tryBatchSubstitute :: String ->? String
-tryBatchSubstitute = wrap $ const Nothing -- TODO
+tryBatchSubstitute = empty -- TODO
 
 trySingleSubstitutions :: String ->? String
 trySingleSubstitutions = execWriter do
