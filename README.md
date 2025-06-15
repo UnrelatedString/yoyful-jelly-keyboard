42a08b5ff8e685d7dae6d25dfbdc087c1cf0f745
UnrelatedString <unrelatedstring@gmail.com>
2025-06-15 12:34:18 -0400

    omfg did i not even have ci?????

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![CI](https://github.com/UnrelatedString/yoyful-jelly-keyboard/actions/workflows/ci.yml/badge.svg?branch=main)](https://github.com/UnrelatedString/yoyful-jelly-keyboard/actions/workflows/ci.yml)
+
 # yoyful-jelly-keyboard
 
 A less broken and more flexible Jelly keyboard bookmarklet
--- src/Jelly/Builtins.purs
+++ src/Jelly/Builtins.purs
@@ -1,10 +1,5 @@
 module Jelly.Builtins
- ( Builtin(..)
+  ( Builtin(..)
+  , builtin
+  ) where
 
- ) where
-
-data Builtin = Builtin {
-    -- ...I can't remember what this was going to be at all ughhhhh
-    -- or wait maybe this was going to be an enum and then stuff maps to/from BuiltinForms?
-    -- probably cleaner overall to just. not do that. and let them come together naturally
-}
