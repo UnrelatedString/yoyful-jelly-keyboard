28b56da8f12d00c6442a3338f780d96bc5f477ee
UnrelatedString <unrelatedstring@gmail.com>
2025-06-18 07:53:45 -0400

    ...wait is this even cleaner than just doing them all in one function?? i think not actually

--- src/Jelly/Builtins.purs
+++ src/Jelly/Builtins.purs
@@ -1,5 +1,6 @@
 module Jelly.Builtins
   ( Builtin(..)
+  , Builtin'
   , BuiltinType(..)
   , Adicity(..)
   , builtin
@@ -7,10 +8,13 @@ module Jelly.Builtins
   ) where
 
 import Prelude
+import Control.Alternative ((<|>))
 import Data.Maybe (Maybe(..))
 import Jelly.Codepage (Jel(..), BuiltinForm(..))
 
-newtype Builtin = Builtin
+data Builtin = Builtin BuiltinType Builtin'
+
+type Builtin' =
   { mnemonic :: Maybe String
   , keywords :: Array String
   , originalDescription :: String
@@ -20,13 +24,22 @@ newtype Builtin = Builtin
 data BuiltinType
   = Atom Adicity
   | Quick -- TODO: systematically represent quick argument counts and how they get adicity
-  | Separator
   | Syntax
 
 data Adicity = Niladic | Monadic | Dyadic
 
 builtin :: BuiltinForm -> Maybe Builtin
-builtin = const Nothing
+builtin form
+  =   (nilads1 form <#> Builtin (Atom Niladic))
+  <|> (monads1 form <#> Builtin (Atom Monadic))
+  <|> (dyads1 form <#> Builtin (Atom Dyadic))
+  <|> (nilads2 form <#> Builtin (Atom Niladic))
+  <|> (monads2 form <#> Builtin (Atom Monadic))
+  <|> (dyads2 form <#> Builtin (Atom Dyadic))
+  <|> (quicks form <#> Builtin Quick)
+  <|> (syntaxConstructs form <#> Builtin Syntax)
+
+nilads1 ::
 
 -- separate from builtin so I don't have to build that "is this also a terminator??"
 -- into the builtin data itself lmao
